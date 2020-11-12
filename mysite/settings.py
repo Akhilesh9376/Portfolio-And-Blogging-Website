0b3d7202daec58264e137bb44a21aea29d0fc735
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2020-11-12 10:20:29 +0530

    Commited

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_MYSITE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_MYSITE')=='True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_MYSITE')=='True')
+# DEBUG = True
 
-# ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS_MYSITE')]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS_MYSITE')]
+# ALLOWED_HOSTS = []
 
 # Application definition
 
