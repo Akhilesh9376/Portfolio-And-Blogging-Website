b7824126d06ba83e1a044165a8d89418c1d2a3c4
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2020-11-14 10:12:35 +0530

    Final

--- main/views.py
+++ main/views.py
@@ -51,8 +51,7 @@ def contact(request):
         EmailMessage(
             'New message from %s' %name,
             'Hi admin, new message form this email address: %s\n\n Message: %s' %(email, message),
-            conf_settings.EMAIL_HOST_USER,
-            ['akhileshyadavkoktaa@gmail.com', ],
+            
         ).send()
 
         form.save()
